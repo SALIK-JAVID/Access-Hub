eabae9fae6df723bab9625f653d758a052889bc0
SALIK-JAVID <salikjaveeddar@gmail.com>
2026-01-14 10:37:01 +0530

    admin feature will be added

---
+++ client/src/pages/Admin.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Admin = () => {
+  return (
+    <div className='min-h-screen flex flex-col items-center justify-center'>
+        <h1 className='text-3xl font-bold mb-4'>
+            Access Hub : Admin Portal
+        </h1>
+        <p className="mb-6">Welcome Admin </p>
+
+     </div>
+  )
+}
+
+export default Admin
+
