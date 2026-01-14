b807b66a3c2a089c8fdf9a75fa6f66a4a853647c
SALIK-JAVID <salikjaveeddar@gmail.com>
2026-01-14 14:07:27 +0530

    some changes in the admin routes

---
+++ server/routes/adminRoutes.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const router = express.Router();
+const {
+    getAllUsers,
+    blockUser,
+    unblockUser,
+} =require("../controllers/adminController");
+
+router.get("/users",getAllUsers)
+router.put("block:/id",blockUser)
+router.put("unblock:/id",unblockUser)
+module.exports = router;
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -6,17 +6,11 @@ const {
     registerUser,
     loginUser
 } = require("../controllers/authController");
-const{
-    getAllUsers,
-    blockUser,
-    unblockUser
-} = require("../controllers/adminController");
+
 
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.post("/users",getAllUsers);
-router.put("/block/:id", blockUser);
-router.put("/unblock/:id",unblockUser);
+
 
 module.exports = router;
