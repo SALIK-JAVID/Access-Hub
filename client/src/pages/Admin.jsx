fdf6bea86f8ac2a4ee52b8f6e177b1edad5a9b5c
SALIK-JAVID <salikjaveeddar@gmail.com>
2026-01-15 11:15:27 +0530

    added pagination logic in the frontend

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -6,6 +6,9 @@ import { toast } from "react-toastify";
 const Admin = () => {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
+  // adding the pagination logic here!
+  const[currentPage, setCurrentPage] = useState(1);
+  const usersPerPage = 2;
 
   const fetchUsers = async () => {
     try {
@@ -43,16 +46,49 @@ const Admin = () => {
   };
 
   if (loading) return <p className="p-4">Loading users...</p>;
+// pagination logic :
+  const indexOfLastUser = currentPage * usersPerPage;
+  const indexOfFirstUser = indexOfLastUser - usersPerPage;
+
+  const currentUsers = users.slice(
+  indexOfFirstUser,
+  indexOfLastUser
+  ); //user.slice(0,2) for first page.
+
+const totalPages = Math.ceil(users.length / usersPerPage);
+
 
   return (
     <div className="p-6">
       <h1 className="text-2xl font-bold mb-4">Access Hub : 
       Admin Panel</h1>
       <UserTable
-        users={users}
+        users={currentUsers}
         onBlock={handleBlock}
         onUnblock={handleUnblock}
       />
+      <div className="flex items-center gap-4 mt-4">
+  <button
+    onClick={() => setCurrentPage((prev) => prev - 1)}
+    disabled={currentPage === 1}
+    className="px-4 py-2 bg-gray-300 rounded disabled:opacity-50"
+  >
+    Previous
+  </button>
+
+  <span className="font-semibold">
+    Page {currentPage} of {totalPages}
+  </span>
+
+  <button
+    onClick={() => setCurrentPage((prev) => prev + 1)}
+    disabled={currentPage === totalPages}
+    className="px-4 py-2 bg-gray-300 rounded disabled:opacity-50"
+  >
+    Next
+  </button>
+</div>
+
     </div>
   );
 };
