2463b7c7abaf91dd431680bffb4ca1455a395b55
SALIK-JAVID <salikjaveeddar@gmail.com>
2026-01-20 15:09:59 +0530

    added lazy loading and infinite scroll in the dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
+import { useRef } from 'react'; //adding lazy loading and infinite scroll effect.
+import { useEffect } from 'react';
+import { useState } from 'react';
 const Dashboard = () => {
+const [count, setCount] = useState(10); //ten initally
+const loaderRef = useRef(null);
+
     const navigate = useNavigate();
 
     const logout = () => {
@@ -11,9 +17,26 @@ const Dashboard = () => {
       const profile = () =>{
         navigate("/profile");
       };   
+  //  lazy loaading and infinite scroll: logic
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          setCount( count + 10 ); // load 10 more
+        }
+      },
+      { threshold: 1 }
+    );
 
+    if (loaderRef.current) {
+      observer.observe(loaderRef.current); //that value 
+    }
+
+    return () => observer.disconnect();   //stop watching the elements.
+  }, []);
   return (
     <>
+    
     <div className='min-h-screen flex flex-col items-center justify-center'>
         <h1 className='text-3xl font-bold mb-4'>
             Access Hub
@@ -31,8 +54,26 @@ const Dashboard = () => {
       >
         Profile
       </button>
+      {/* Displaying Avatar from dice-bear */}
+       <div className='p-6'>
+       <h1 className='text-xl font-bold mb-4'>User Avatars</h1>
+       <div className="grid grid-cols-5 gap-4">
+        {Array.from({ length: count }).map((_, index) => (
+          <img
+            key={index}
+            src={`https://api.dicebear.com/7.x/avataaars/svg?seed=${index}`}
+            alt="avatar"
+            className="w-20 h-20"
+          />
+        ))}
+      </div>
 
-        </div>
+      {/* This div triggers loading more */}
+      <div ref={loaderRef} className="h-10"></div>
+
+      </div>
+
+    </div>
     </>
     
   )
